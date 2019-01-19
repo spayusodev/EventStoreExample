14c829874f2bb337e14d24df8ff2d935f8cba5b5
pajueloayuso <pajueloayuso@gmail.com>
2019-01-19 23:16:58 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventStoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
